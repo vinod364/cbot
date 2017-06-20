27bcae4de799ec00bfa262e36c5102379ff30b50
Vinod Kumar Sharma <vinodks@Vinods-MacBook-Pro-7.local>
2017-06-20 18:05:19 +0530

    Fixed syntax error

--- app.js
+++ app.js
@@ -186,7 +186,7 @@ io.on('connection', function (socket) {
 			setTimeout(function () {
 				socket.emit('chat message', {
 					roomID: data.roomID,
-					msg:JSON.stringify(data.msg);
+					msg:JSON.stringify(data.msg),
 					//msg: JSON.stringify(apiData[0]),
 					timestamp: time,
 					isAdmin: true
